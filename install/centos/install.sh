0af696aba9cf358eb939b6f5955f8c3636379a9e
Leandro Rosa <leandrorosa85@gmail.com>
2020-04-17 13:26:27 +0200

    fixed omf install

--- install/centos/install.sh
+++ install/centos/install.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+sudo yum groupinstall 'Development Tools' -y
+echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> /home/centos/.bash_profile
+eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+brew install gcc
+
+#dev
+brew install bat
+brew install docker
+brew install fish
+brew install go
+brew install htop
+brew install terraform
+
+
 cd ~/install/centos/
 sudo yum update
 sudo yum -y install $(cat packages)
