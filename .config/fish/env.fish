3354f9e7b2bc2ae4d9999c7d3e4139a3370b940f
Leandro Rosa <leandrorosa85@gmail.com>
2020-04-17 12:37:58 +0200

    added puppet provisioner

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -4,8 +4,11 @@ set -gx GOPATH ~/Code/Go
 mkdir -p "$GOPATH/bin"
 mkdir -p "$GOPATH/src"
 
+set -gx PROVISIONER_HOME "/tmp/puppet-provisioner"
+mkdir -p "$PROVISIONER_HOME/bin"
+
 # Path
 if not set -q -U fish_user_paths
-  set -U fish_user_paths "$HOME/bin" "$GOPATH/bin" "$HOME/.local/bin"
+  set -U fish_user_paths "$HOME/bin" "$GOPATH/bin" "$HOME/.local/bin" "$PROVISIONER_HOME/bin"
 end
 
