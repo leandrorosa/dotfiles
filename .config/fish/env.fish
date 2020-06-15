35ade1f41585c8cc2cab21defd2ad365a160e333
Leandro Rosa <leandrorosa85@gmail.com>
2020-06-15 09:38:39 +0200

    added anaconda

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -1,14 +1,9 @@
-#set -Ux RUBY_CONFIGURE_OPTS "--with-openssl-dir=$(brew --prefix openssl@1.1)"
-
 set -gx GOPATH ~/Code/Go
 mkdir -p "$GOPATH/bin"
 mkdir -p "$GOPATH/src"
 
-set -gx PROVISIONER_HOME "/tmp/puppet-provisioner"
-mkdir -p "$PROVISIONER_HOME/bin"
-
-# Path
-if not set -q -U fish_user_paths
-  set -U fish_user_paths "$HOME/bin" "$GOPATH/bin" "$HOME/.local/bin" "$PROVISIONER_HOME/bin"
-end
+set -U fish_user_paths "$HOME/bin" $fish_user_paths
+set -U fish_user_paths "$GOPATH/bin" $fish_user_paths
+set -U fish_user_paths "$HOME/.local/bin" $fish_user_paths
+set -U fish_user_paths "$HOME/anaconda3/bin" $fish_user_paths
 
