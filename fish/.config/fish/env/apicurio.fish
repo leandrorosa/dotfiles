7c11e8ffe8dc2fc5a98f7e9cd5e11722880829b6
Leandro Rosa <leandro.rosa@4screen.com>
2025-03-26 11:53:50 +0100

    add completions

--- fish/.config/fish/env/apicurio.fish
+++ fish/.config/fish/env/apicurio.fish
@@ -1,8 +1,22 @@
-set -e APICURIO_CLIENT_ID
-set -e APICURIO_CLIENT_SECRET
-
-function refresh-apicurio-envs --description 'Configure Apicurio environment variables'
-    set -Ux APICURIO_CLIENT_ID (vault kv get --field="client-id.pipeline" secret/general-secrets/unmanaged/apicurio)
-    set -Ux APICURIO_CLIENT_SECRET (vault kv get --field="client-secret.pipeline" secret/general-secrets/unmanaged/apicurio)
+function lazy_set_apicurio_client_id
+    if not set -q APICURIO_CLIENT_ID
+        set -Ux APICURIO_CLIENT_ID (vault kv get --field="client-id.pipeline" secret/general-secrets/unmanaged/apicurio)
+    end
 end
 
+function lazy_set_apicurio_client_secret
+    if not set -q APICURIO_CLIENT_SECRET
+        set -Ux APICURIO_CLIENT_SECRET (vault kv get --field="client-secret.pipeline" secret/general-secrets/unmanaged/apicurio)
+    end
+end
+
+function check_and_set_apicurio_vars
+    if string match -r '.*\$APICURIO_CLIENT_ID.*' (commandline -p)
+        lazy_set_apicurio_client_id
+    end
+    if string match -r '.*\$APICURIO_CLIENT_SECRET.*' (commandline -p)
+        lazy_set_apicurio_client_secret
+    end
+end
+
+check_and_set_apicurio_vars
