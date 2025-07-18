7a1eb465f073385461a86f53e20c8e87d8f46646
Leandro Rosa <leandro.rosa@4screen.com>
2025-07-18 16:24:05 +0200

    add asdf

--- asdf-installer.sh
+++ asdf-installer.sh
@@ -7,6 +7,7 @@ asdf plugin add azure-cli https://github.com/EcoMind/asdf-azure-cli
 asdf plugin add java https://github.com/halcyon/asdf-java.git
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin add python https://github.com/danhper/asdf-python.git
+asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
 
 # kubernetes
 asdf plugin add k9s
