c404542f806c45bd2101df50acb2378f40009fe1
Leandro Rosa <leandro.rosa@4screen.com>
2025-07-18 16:21:17 +0200

    add asdf

--- asdf-installer.sh
+++ asdf-installer.sh
@@ -1,4 +1,18 @@
+
+# cloud
+asdf plugin add terraform
 asdf plugin add azure-cli https://github.com/EcoMind/asdf-azure-cli
+
+# languages
 asdf plugin add java https://github.com/halcyon/asdf-java.git
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin add python https://github.com/danhper/asdf-python.git
+
+# kubernetes
+asdf plugin add k9s
+adsf plugin add kubectl
+adsf plugin add kubectx
+adsf plugin add minikube
+
+# tools
+asdf plugin add task
---
+++ fish/.config/fish/env/task.fish
@@ -0,0 +1,2 @@
+task --completion fish | source
+
