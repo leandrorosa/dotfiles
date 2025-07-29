b194a4e907c7d6fb14ba44405a33a91b11241eae
Leandro Rosa <leandro.rosa@4screen.com>
2025-07-29 09:50:26 +0200

    disable starship

--- asdf-installer.sh
+++ asdf-installer.sh
@@ -5,6 +5,7 @@ asdf plugin add azure-cli https://github.com/EcoMind/asdf-azure-cli
 
 # languages
 asdf plugin add java https://github.com/halcyon/asdf-java.git
+asdf plugin add maven  https://github.com/halcyon/asdf-maven
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin add python https://github.com/danhper/asdf-python.git
 asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
